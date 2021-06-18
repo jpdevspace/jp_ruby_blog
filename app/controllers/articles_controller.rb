7e8ec75e65e78d316b878ce7b5a3c3a5c480103f
Jean Paul <jpgiraldo7@gmail.com>
2021-06-18 10:18:53 -0400

    can successfuly create articles and displays error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,9 +9,26 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      # this is the full code, but we're going to use the shorthand version
+      # redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render "new"
+    end
   end
+
+  # Strong Typing: this method filters params (allowed list)
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
 
 end
