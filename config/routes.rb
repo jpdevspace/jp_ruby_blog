1098dcbc3da258d057229429f5c85e8c5f882829
Jean Paul <jpgiraldo7@gmail.com>
2021-06-18 07:30:37 -0400

    added new actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get "/about", to: "pages#about"
   
-  # All articles
-  resources :articles, only: [:show, :index]
+  # This is the same as handling each CRUD operation
+  # on a separate line, something like
+  # get "/articles", to: "articles#index" => Show ALL articles
+  # get "/articles/:id", to: "articles#show" => Show ONE article
+
+  resources :articles, only: [:show, :index, :new, :create]
 end
